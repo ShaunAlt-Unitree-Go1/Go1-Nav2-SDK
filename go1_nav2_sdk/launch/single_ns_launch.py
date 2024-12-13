a73c31265eb755cbdc9506c2773d2ca80e7b3740
Shaun Altmann <shaunalt10@gmail.com>
2024-12-13 14:37:21 +1100

    removed `TextSubstitution` from single_ns_launch.py

--- go1_nav2_sdk/launch/single_ns_launch.py
+++ go1_nav2_sdk/launch/single_ns_launch.py
@@ -51,7 +51,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(path_nav2, 'launch', 'rviz_launch.py')),
         condition = IfCondition(rviz),
         launch_arguments = {
-            'namespace': TextSubstitution(namespace),
+            'namespace': namespace,
             'use_namespace': 'true',
             'rviz_config': os.path.join(path_nav2, 'rviz', 'nav2_namespaced_view.rviz'),
         }.items()
@@ -61,8 +61,8 @@ def generate_launch_description():
     node_static_tf = ExecuteProcess(
         cmd = [
             'ros2', 'run', 'tf2_ros', 'static_transform_publisher',
-            '--frame-id', TextSubstitution(namespace) + '/trunk',
-            '--child-frame-id', TextSubstitution(namespace) + '/base_link',
+            '--frame-id', namespace + '/trunk',
+            '--child-frame-id', namespace + '/base_link',
         ],
         output = 'screen'
     )
