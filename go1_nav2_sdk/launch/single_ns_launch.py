70926d4da884fb5934b75e4b6e8744f304fa0de5
Shaun Altmann <shaunalt10@gmail.com>
2024-12-13 16:21:38 +1100

    made r1.yaml the default params

--- go1_nav2_sdk/launch/single_ns_launch.py
+++ go1_nav2_sdk/launch/single_ns_launch.py
@@ -40,7 +40,8 @@ def generate_launch_description():
     declare_params_file = DeclareLaunchArgument(
         'params_file',
         # default_value = os.path.join(path_nav2, 'params', 'nav2_multirobot_params_all.yaml'),
-        default_value = os.path.join(path_nav2, 'params', 'nav2_params.yaml'),
+        # default_value = os.path.join(path_nav2, 'params', 'nav2_params.yaml'),
+        default_value = os.path.join(path_sdk, 'params', 'r1.yaml'),
         description = 'Path to the parameters file for the Go1 Robot.'
     )
     declare_rviz = DeclareLaunchArgument(
