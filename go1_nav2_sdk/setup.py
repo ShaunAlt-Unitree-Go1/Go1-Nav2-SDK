3e2f1d2aca5f0418c6e49616cb487a8b005c6885
Shaun Altmann <shaunalt10@gmail.com>
2024-12-16 12:39:36 +1100

    fixed maps issue

--- go1_nav2_sdk/setup.py
+++ go1_nav2_sdk/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'params'), glob(os.path.join('params', '*'))),
+        (os.path.join('share', package_name, 'maps'), glob(os.path.join('maps', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
