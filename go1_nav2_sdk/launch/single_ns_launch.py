f61d6325012c7d89023c8d1532538612552cc429
Shaun Altmann <shaunalt10@gmail.com>
2024-12-18 09:36:50 +1100

    forced no slam in single_ns_launch.py

--- go1_nav2_sdk/launch/single_ns_launch.py
+++ go1_nav2_sdk/launch/single_ns_launch.py
@@ -70,7 +70,7 @@ def generate_launch_description():
             'map': os.path.join(path_sdk, 'maps', 'map-test2.yaml'),
             'namespace': namespace,
             'params_file': params_file,
-            # 'slam': 'True',
+            'slam': 'False',
             'use_namespace': 'True',
             'use_sim_time': 'True',
         }.items()
